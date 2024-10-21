9796345b64ce51480d980ecda90050f40bffa51f
sofiaWebT <98661058+Sofiamishel2003@users.noreply.github.com>
2024-10-20 22:36:11 -0600

    create_proyect

---
+++ target/debug/deps/model.d
@@ -0,0 +1,5 @@
+C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\3D_Model\target\debug\deps\model.exe: src/main.rs
+
+C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\3D_Model\target\debug\deps\model.d: src/main.rs
+
+src/main.rs:
