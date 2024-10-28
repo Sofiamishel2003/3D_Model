27dde8702b1dfc46edba9a360a987dfdad034365
sofiaWebT <98661058+Sofiamishel2003@users.noreply.github.com>
2024-10-28 16:49:39 -0600

    add model

--- target/debug/deps/model.d
+++ target/debug/deps/model.d
@@ -1,5 +1,13 @@
-C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\3D_Model\target\debug\deps\model.exe: src/main.rs
+C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\3D_Model\target\debug\deps\model.exe: src/main.rs src\framebuffer.rs src\triangle.rs src\line.rs src\vertex.rs src\obj.rs src\color.rs src\fragment.rs src\shaders.rs
 
-C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\3D_Model\target\debug\deps\model.d: src/main.rs
+C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\3D_Model\target\debug\deps\model.d: src/main.rs src\framebuffer.rs src\triangle.rs src\line.rs src\vertex.rs src\obj.rs src\color.rs src\fragment.rs src\shaders.rs
 
 src/main.rs:
+src\framebuffer.rs:
+src\triangle.rs:
+src\line.rs:
+src\vertex.rs:
+src\obj.rs:
+src\color.rs:
+src\fragment.rs:
+src\shaders.rs:
